2ac2c6b8cb997d5d838b8bfc16a035e7e16c8fc7
Ahmet Arda Kavakcı <ahmetardakavakci@gmail.com>
2025-12-30 11:31:23 +0300

    chore(init): Add types and format

--- lua/gruvbox-minimal/init.lua
+++ lua/gruvbox-minimal/init.lua
@@ -1,5 +1,17 @@
+--- @alias GruvboxContrast "low" | "high"
+--- @alias GruvboxTheme "dark" | "light"
+--- @alias GruvboxAccent "red" | "orange" | "yellow" | "green" | "cyan" | "blue" | "magenta"
+
+--- @class GruvboxConfig
+--- @field transparent? boolean
+--- @field italic_comments? boolean
+--- @field contrast? GruvboxContrast
+--- @field theme? GruvboxTheme
+--- @field accent? GruvboxAccent
+
 local M = {}
 
+--- @type GruvboxConfig
 M.config = {
     transparent = false,
     italic_comments = false,
@@ -8,17 +20,18 @@ M.config = {
     accent = "red",
 }
 
+--- @param opts? GruvboxConfig
 function M.setup(opts)
-	M.config = vim.tbl_deep_extend("force", M.config, opts or {})
+    M.config = vim.tbl_deep_extend("force", M.config, opts or {})
 end
 
 function M.load()
-	local palette = require("gruvbox-minimal.palette").get(M.config.contrast, M.config.theme)
-	local groups = require("gruvbox-minimal.groups").setup(palette, M.config)
+    local palette = require("gruvbox-minimal.palette").get(M.config.contrast, M.config.theme)
+    local groups = require("gruvbox-minimal.groups").setup(palette, M.config)
 
-	for group, settings in pairs(groups) do
-		vim.api.nvim_set_hl(0, group, settings)
-	end
+    for group, settings in pairs(groups) do
+        vim.api.nvim_set_hl(0, group, settings)
+    end
 end
 
 return M
